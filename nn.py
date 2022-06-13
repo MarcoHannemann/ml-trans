- fixed a bug where windspeed and canopy height got mixed in calculation of PM model

nn.py
@@ -19,8 +19,6 @@ import metrics
 
 # global to-do list
 # todo: Involve flags in SAPFLUXNET data
-# todo: Full extraction of FPAR for SFN and FLX
-# todo: Involve more SFN sites e.g. other PFTs
 # todo: involve SWVL2?
 #  For example, Zeppel et al. (2008) reported that the transpira-
 #  tion of Australian woodland was independent of the water content of
@@ -74,7 +72,7 @@ def predict(model, x, y):
     return predictions_to_dataframe(y, pred) if y is not None else pred
 
 
-def predict_fluxnet(model):
+def predict_fluxnet(model, target="transpiration"):
     """Uses trained model to predict T at FLUXNET sites.
 
     :param model: Compiled tf.keras model
@@ -95,32 +93,65 @@ def predict_fluxnet(model):
             print(f"Warning! File {file} cannot be reindexed, "
                   f"only has {len(series)} elements, new index has {len(idx)}!")
             continue
+
+        # If target is canopy conductance, apply Penman-Monteith equation on predictions
+        if target == "gc":
+            df = pd.read_csv(file, parse_dates=True)
+            gc = series.copy()
+            gc /= 1000
+            gc.index = df.index
+            series = phys_model.latent_heat_to_evaporation(
+                phys_model.pm_standard(gc=gc, ta=df["t2m"], VPD=df["vpd"], netrad=df["ssr"], LAI=df["LAI"], SZA=0,
+                                       u=df["height"], h=df["height"], z=df["height"]), df["t2m"])
+            series.index = idx
+        # If target is alpha, apply Priestly-Taylor equation
+        elif target == "alpha":
+            df = pd.read_csv(file, parse_dates=True)
+            alpha = series.copy()
+            alpha.index = df.index
+            series = phys_model.latent_heat_to_evaporation(
+                phys_model.pt_standard(ta=df["t2m"], netrad=df["ssr"], LAI=df["LAI"],
+                                       SZA=0, alpha_c=alpha), ta=df["t2m"])
+            series.index = idx
+
+        # Append FLUXNET prediction to data frame
         predictions_all_stations = pd.concat([predictions_all_stations,
                                               series.rename(os.path.basename(file)[:-4])],
                                              axis=1)
         series.plot(lw=0.3)
         plt.savefig(f'output/fluxnet_predictions/fig/{os.path.basename(file)[:-4]}')
         plt.clf()
-    predictions_all_stations.to_csv('output/fluxnet_predictions/flx_predictions.csv')
+
+    # Write out CSV with all FLUXNET predictions
+    predictions_all_stations.to_csv(f'output/fluxnet_predictions/flx_predictions_{target}.csv')
 
 
-# load data and set model options
-features = ["t2m", "ssr", "swvl1", "vpd", "windspeed", "IGBP", "height", "LAI", "FPAR"]
+# model settings
+features = ["t2m", "swvl1", "vpd", "windspeed", "IGBP", "height", "LAI", "FPAR"]
+target = "transpiration"
+frequency = "1D"
 
-
+# model architecture
 layers = 5
 neurons = 256
-dropout_rate = False
+dropout_rate = 0.1
 act_fn = "selu"
 
 ext_path = "data/fluxnet_lai"
 #ext_path = None
 
 # load model data and create sequential model
-train_data, metadata = load_model_data.load(path_csv="data/param/", freq="1D", features=features,
-                                            blacklist="whitelist.csv", target="transpiration",
+train_data, metadata = load_model_data.load(path_csv="data/param/", freq=frequency, features=features,
+                                            blacklist="whitelist.csv", target=target,
                                             external_prediction=ext_path)
-upper_lim = 25 # 10**(math.ceil(math.log(train_data["Ytrain"].max(), 10))) // 5
+
+# Scale canopy conductance so make NN converge faster
+if target == "gc":
+    train_data["Ytrain"] *= 1000
+    train_data["Ytest"] *= 1000
+    train_data["Yval"] *= 1000
+
+upper_lim = 10  # ** (math.ceil(math.log(train_data["Ytrain"].max(), 10)))
 input_shape = train_data["Xtrain"].shape[1]
 model = create_model(inp_shape=input_shape,
                      activation=act_fn,
@@ -130,7 +161,7 @@ model = create_model(inp_shape=input_shape,
 
 # Callbacks
 # Early Stopping if validation loss doesn't change within specified number of epochs
-es_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=200)
+es_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=500)
 
 # Store model parameters
 model_time = datetime.now().strftime("%Y%m%d_%H:%M:%S")
@@ -145,6 +176,8 @@ model.fit(train_data["Xtrain"], train_data["Ytrain"], epochs=5000, batch_size=50
 # todo: load pretrained model
 # checkpoint_path = "??"
 # model.load_weights(checkpoint_path)
+
+# Save trained model to disk
 model.save(f"models/{model_time}")
 
 # apply trained model on training data
@@ -152,17 +185,25 @@ df_train = predict(model, train_data["Xtrain"], train_data["Ytrain"])
 df_test = predict(model, train_data["Xtest"], train_data["Ytest"])
 df_val = predict(model, train_data["Xval"], train_data["Yval"])
 
+# Use model to predict T at FLUXNET sites
+if ext_path:
+    predict_fluxnet(model, target=target)
 
-"""x = train_data["untransformed"]["Xtrain"].reset_index()
-T = phys_model.pm_standard(gc=df_train["y_pred"], ta=x["t2m"], VPD=x["vpd"], netrad=x["ssr"], LAI=x["LAI"], SZA=0, u=x["height"], h=x["height"], z=x["height"], )
-c = pd.concat([x["transpiration"],
-               phys_model.latent_heat_to_evaporation(T, train_data["untransformed"]["Xtrain"]["t2m"].to_numpy())],
-              axis=1)
-c.columns = ["true", "pred"]
-print(r2_score(c["true"], c["pred"]))
-c.plot(kind="scatter", x="pred", y="true", xlim=(0, 20), ylim=(0, 20))
-plt.show()"""
-
+if target == "gc":
+    x = train_data["untransformed"]["Xtrain"].reset_index()
+    T = phys_model.pm_standard(gc=df_train["y_pred"]/1000, ta=x["t2m"], VPD=x["vpd"], netrad=x["ssr"], LAI=x["LAI"], SZA=0,
+                               u=x["windspeed"], h=x["height"], z=x["height"], )
+    t_true = phys_model.pm_standard(gc=df_train["y_true"]/1000, ta=x["t2m"], VPD=x["vpd"], netrad=x["ssr"], LAI=x["LAI"],
+                                    SZA=0,
+                                    u=x["windspeed"], h=x["height"], z=x["height"], )
+    c = pd.concat(
+        [phys_model.latent_heat_to_evaporation(t_true, x["t2m"].to_numpy()),
+         phys_model.latent_heat_to_evaporation(T, x["t2m"].to_numpy())],
+        axis=1)
+    c.columns = ["true", "pred"]
+    print(r2_score(c["true"], c["pred"]))
+    c.plot(kind="scatter", x="pred", y="true", xlim=(0, 100), ylim=(0, 100), s=0.3)
+    plt.show()
 
 # visualize model results in a scatter plot for training, testing, validation
 plotting.scatter_density_plot(df_train, df_test, df_val,
@@ -196,6 +237,3 @@ metadata["results"]["cpk_path"] = f"checkpoint/{model_time}/"
 with open(f"models/{model_time}.json", "w") as fp:
     json.dump(metadata, fp, indent=1)
 
-# Use model to predict T at FLUXNET sites
-if ext_path:
-    predict_fluxnet(model)