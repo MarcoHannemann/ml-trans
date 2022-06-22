b87f454ba6db9160817dfea836980793d4c75a2e
Marco Hannemann <marco.hannemann@mailbox.org>
2022-06-22 10:28:03 +0200

    Revised physical model. - Introduced calculation of psychrometric constant - Fixed conversion of ET and LE for daily and hourly time scales - Improved docstrings and references - revised constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,12 @@
 karmann = 0.41
+# Density of air [-]
 air_density = 1.2
-air_specific_heat_capacity = 1.013
-latent_heat_vaporization = 2450
-psychrometric_constant = 0.000665
+
+# specific heat at constant pressure ]MJ kg-1 °C-1]
+air_specific_heat_capacity = 1.013*10**(-3)
+
+# Ratio of molecular weight of water vapor and dry air [-]
+molecular_water_air_ratio = 0.622
+
 # extinction coefficient for moderate to dense vegetated cover (F>2)
 k = 0.45
