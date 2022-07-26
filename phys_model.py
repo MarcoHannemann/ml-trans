Fixed sunrise/sunset calculation for sites / days with 24 hours sunlight

phys_model.py
@@ -5,7 +5,7 @@ This module contains all physical equations for the Priestley-Taylor and the Pen
 Can be run as stand-alone for creating training target data by inverting PT or PM.
 """
 
-# todo: Check RuntimeWarning in np.sqrt(2 * np.cos(SZA))), faulty SZA?
+# todo: Check RuntimeWarning in np.sqrt(2 * np.cos(SZA))), negative SZA? Use radians!
 import numpy as np
 import pandas as pd
 import solar
@@ -93,7 +93,7 @@ def canopy_available_energy(netrad, LAI, SZA):
     :return: Ac: Canopy available energy
     """
 
-    Ac = netrad * (1 - np.exp(-0.5 * LAI / np.cos((SZA))))
+    Ac = netrad * (1 - np.exp(-0.5 * LAI / np.cos(SZA)))
     return Ac
 
 
@@ -107,7 +107,7 @@ def net_radiation_canopy(netrad, LAI, SZA):
         temperature: A dual-temperature-difference method to minimize measurement errors.
         In Water Resources Research (Vol. 36, Issue 8, pp. 2263–2274). American Geophysical Union (AGU)."""
 
-    r_nc = netrad * (1 - np.exp(-constants.k * LAI / np.sqrt(2 * np.cos(SZA))))
+    r_nc = netrad * (1 - np.exp(-constants.k * LAI / np.sqrt(2 * np.cos(np.radians(SZA)))))
     return r_nc
 
 
@@ -219,17 +219,20 @@ def pt_inverted(ta, p, netrad, LAI, SZA, T):
 if __name__ == "__main__":
     sites = pd.read_csv("site_meta.csv", index_col=0)
     for site in list(sites.index):
-        print(site)
         try:
             df = pd.read_csv(f"~/Projects/ml-trans/data/sapfluxnet_daily/{site}.csv", index_col=0, parse_dates=True)
         except FileNotFoundError:
             continue
         df = df.dropna()
+        if len(df) == 0:
+            print(f"Data missing for site {site}")
+            continue
         latitude = sites[sites.index == site]["lat"].item()
         longitude = sites[sites.index == site]["lon"].item()
         day_series = pd.Series(df.index)
         day_series = day_series.apply(lambda day: solar.hogan_sza_average(lat=latitude, lon=longitude, date=day))
         zenith_angle = np.degrees(np.arccos(day_series))
+
         zenith_angle.index = df.index
         try:
             gc = pm_inverted(T=evaporation_to_latent_heat(df["transpiration"], df["t2m"]), p=df["sp"], ta=df["t2m"],
@@ -239,7 +242,7 @@ if __name__ == "__main__":
         except KeyError:
             continue
         try:
-            alpha = pt_inverted(ta=df["t2m"], p=df["sp"], netrad=df["ssr"], LAI=df["LAI"], SZA=np.array(zenith_angle),
+            alpha = pt_inverted(ta=df["t2m"], p=df["sp"], netrad=df["ssr"], LAI=df["LAI"], SZA=zenith_angle,
                                 T=evaporation_to_latent_heat(df["transpiration"], df["t2m"]))
         except KeyError:
             continue

solar.py
@@ -10,7 +10,7 @@ from timezonefinder import TimezoneFinder
 from nptyping import NDArray, Shape
 from typing import Any
 
-
+# todo: make functions work with scalars where possible (e.g. solar dec)
 class Location:
     """Class Location to compute earth-sun relationships at a location on a given date."""
     lat: float
@@ -166,13 +166,33 @@ class Location:
         )
 
     def calc_sunrise_sunset(self):
-        """Calculates sunrise at sunset as pd.Timestamp."""
+        """Calculates sunrise at sunset as pd.Timestamp.
+
+        Note that for e.g. days without night time at high latitudes, sunrise/sunset cannot be calculated. In this case,
+        _sunrise_hour_angle() returns NaN or values greater than 1. We set sunrise to 1 minutes before the first time
+        step and sunset to 1 minute after the last time step respectively in order to not mask out any night values."""
+
         solnoon = self._solar_noon()
         sunrise_hour_angle = self._sunrise_hour_angle()
         sunrise_decimal = 24 * (solnoon - sunrise_hour_angle * 4 / 1440)
         sunset_decimal = 24 * (solnoon + sunrise_hour_angle * 4 / 1440)
-        sunrises = []
-        sunsets = []
+
+        if np.isnan(sunrise_decimal[0]) and sunrise_decimal[0] > 1 and sunset_decimal[0] > 24:
+            self.sunrise = (self.local_date[0].replace(hour=int(sunrise_decimal[0]),
+                                                       minute=int((sunrise_decimal[0] * 60) % 60),
+                                                       second=int((sunrise_decimal[0] * 3600) % 60)
+                                                       ))
+
+            self.sunset = (self.local_date[0].replace(hour=int(sunset_decimal[0]),
+                                                      minute=int((sunset_decimal[0] * 60) % 60),
+                                                      second=int((sunset_decimal[0] * 3600) % 60)
+                                                      ))
+
+        else:
+            # No day/nighttime
+            self.sunrise = self.local_date[0] - pd.to_timedelta("1min")
+            self.sunset = self.local_date[-1] + pd.to_timedelta("1min")
+        """
         for enum, day in enumerate(self.local_date):
             try:
                 int(sunrise_decimal[enum])
@@ -188,9 +208,9 @@ class Location:
             sunsets.append(day.replace(hour=int(sunset_decimal[enum]),
                                        minute=int((sunset_decimal[enum] * 60) % 60),
                                        second=int((sunset_decimal[enum] * 3600) % 60)
-                                       ))
-        self.sunrise = sunrises
-        self.sunset = sunsets
+                                       ))"""
+        # self.sunrise = sunrises
+        # self.sunset = sunsets
 
 
 def hogan_sza_average(lat: float, lon: float, date: pd.Timestamp) -> float: